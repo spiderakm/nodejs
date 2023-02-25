08717541abe54635e69de782f2549ec02dd243a5
ashok kumar <spider.akm@gmail.com>
2023-02-25 09:51:06 +0530

    playing with different routes

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var http = require('http');
 
 var server = http.createServer((req,res) => {
-    res.write('ashok kumar');
-    res.end()
+    const url = req.url;
+    if(url ==='/home'){
+        res.end("welcome home")
+    }
+    if(url ==='/about'){
+        res.end("welcome to about page")
+        
+    }
+    if(url ==='/node'){
+        res.end("welcome to my node project")
+    }
 })
 server.listen(4000)
